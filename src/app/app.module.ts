6914e451dd7dcdfd9ee23cc0bcbea384db7f24ab
oleksandr.berezhnyi <oleksandr_berezhnyi_tp@bmc.com>
2017-03-07 12:15:01 +0200

    Add themes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ThemesComponent } from './themes/themes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ThemesComponent
   ],
   imports: [
     BrowserModule,
