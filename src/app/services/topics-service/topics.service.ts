e6bf335a8d50db7e2622010d20ffcc1562c6e3d6
oleksandr.berezhnyi <oleksandr_berezhnyi_tp@bmc.com>
2017-03-07 16:08:24 +0200

    Add topics service

---
+++ src/app/services/topics-service/topics.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class TopicsService {
+
+  constructor(private http: Http) { }
+
+  getTopics() {
+    return this.http.get('https://uctalks.herokuapp.com/')
+        .map(response => response.json());
+  }
+
+}
