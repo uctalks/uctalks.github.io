fe3a9b29cd56412aa6c9a22e8237695e569c73f1
Yevhen Stativka <yavhen_stativka_tp@bmc.com>
2017-03-07 17:57:03 +0200

    Fix Auth0 service compilation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
 
-import { AUTH_PROVIDERS } from 'angular2-jwt';
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,6 +11,10 @@ import { ThemesComponent } from './themes/themes.component';
 
 import { TopicsService } from './services/topics-service/topics.service';
 import { Auth } from './auth.service';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp( new AuthConfig({}), http, options);
+}
 
 @NgModule({
   declarations: [
@@ -25,7 +29,11 @@ import { Auth } from './auth.service';
   ],
   providers: [
     TopicsService,
-    AUTH_PROVIDERS,
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [ Http, RequestOptions ]
+    },
     Auth
   ],
   bootstrap: [AppComponent]
