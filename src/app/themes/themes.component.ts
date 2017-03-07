734c94dec4d91272ec1245c4021da147f5e1a04a
Alexandr Berezhnoy <danteisback@bigmir.net>
2017-03-07 16:49:59 +0200

    Theme container (#6)
    * Add themes component
    
    * Add topics service
    
    * Make possible to get topics
    
    * Delete test file

--- src/app/themes/themes.component.ts
+++ src/app/themes/themes.component.ts
@@ -1,31 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Theme } from './theme'
+import { Component, OnInit } from "@angular/core";
+import { TopicsService } from "../services/topics-service/topics.service";
 
 @Component({
-  selector: 'app-themes',
-  templateUrl: './themes.component.html',
-  styleUrls: ['./themes.component.scss']
+    selector: 'app-themes',
+    templateUrl: './themes.component.html',
+    styleUrls: [ './themes.component.scss' ],
 })
 export class ThemesComponent implements OnInit {
-  themes: Theme[];
+    themes: Object[];
 
-  constructor() {
-    this.themes = [
-        new Theme (
-            'Theme 1'
-        ),
-        new Theme (
-            'Theme 2'
-        ),
-        new Theme (
-            'Theme 3'
-        )
-    ];
-      console.log(this.themes);
-  }
+    constructor( public topicsService: TopicsService ) {
+        this.themes = [];
+        console.log(topicsService.getTopics());
+        topicsService.getTopics()
+            .subscribe(
+                response => {
+                    this.themes = response;
+                    console.log(this.themes);
+                },
+                error => console.log(error),
+                () => console.log('done')
+            );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
