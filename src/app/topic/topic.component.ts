ad1963478db6a627795a49de4dd98b98bcdb9ca0
oleksandr.berezhnyi <oleksandr_berezhnyi_tp@bmc.com>
2017-03-09 16:01:56 +0200

    Disable infinity voting

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -8,15 +8,21 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TopicComponent implements OnInit {
   @Input() topic: Object[];
   points: number = 0;
+  voteCheck: boolean = false;
 
   voteUp() {
-    this.points++;
+    if (!this.voteCheck) {
+      this.points++;
+      this.voteCheck = true;
+    }
+
     return false;
   }
 
   voteDown() {
     if (this.points !== 0) {
       this.points--;
+      this.voteCheck = false;
     }
 
     return false;
