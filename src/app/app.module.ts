4ce8e2e4b3b28e6573ec4e08dda35eeba6b37427
oleksandr.berezhnyi <oleksandr_berezhnyi_tp@bmc.com>
2017-03-09 12:22:05 +0200

    Add topic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ThemesComponent } from './themes/themes.component';
+import { TopicsComponent } from './topics/topics.component';
+import { TopicComponent } from './topic/topic.component';
 
 import { TopicsService } from './services/topics-service/topics.service';
 import { Auth } from './auth.service';
@@ -20,7 +21,8 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   declarations: [
     AppComponent,
     HeaderComponent,
-    ThemesComponent
+    TopicsComponent,
+    TopicComponent
   ],
   imports: [
     BrowserModule,
