5ad3707db9f8eb58e4c8c6f222e305a25abe5169
Eugene Stativka <stativkaea@gmail.com>
2017-05-13 11:20:11 +0300

    AOT and updated packages (#13)
    * Update dependencies
    
    * Add AOT build and update packages
    
    * Downgrade zone.js to 0.8.5

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import Topic from './topic.interface';
 
 @Component({
   selector: 'app-topic',
@@ -6,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./topic.component.scss']
 })
 export class TopicComponent implements OnInit {
-  @Input() topic: Object[];
+  @Input() topic: Topic;
   points: number = 0;
   myVote: number = 0;
 
