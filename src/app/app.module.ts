90d5308b7098a66a4c296122d667f59d1b80815b
oleksandr.berezhnyi <oleksandr_berezhnyi_tp@bmc.com>
2017-05-13 13:49:34 +0300

    Add popup, add option to add topics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule, Http, RequestOptions } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Http, HttpModule, RequestOptions} from '@angular/http';
 
-import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
+import {AuthConfig, AuthHttp} from 'angular2-jwt';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { TopicsComponent } from './topics/topics.component';
-import { TopicComponent } from './topic/topic.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {TopicsComponent} from './topics/topics.component';
+import {TopicComponent} from './topic/topic.component';
+import {TopicPopupComponent} from './topic-popup/topic-popup.component';
 
-import { TopicsService } from './services/topics-service/topics.service';
-import { Auth } from './auth.service';
+import {TopicsService} from './services/topics-service/topics.service';
+import {Auth} from './auth.service';
 
-import { SimpleNotificationsModule } from 'angular2-notifications';
+import {SimpleNotificationsModule} from 'angular2-notifications';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
-  return new AuthHttp( new AuthConfig({}), http, options);
+  return new AuthHttp(new AuthConfig({}), http, options);
 }
 
 @NgModule({
@@ -24,7 +25,8 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
     AppComponent,
     HeaderComponent,
     TopicsComponent,
-    TopicComponent
+    TopicComponent,
+    TopicPopupComponent
   ],
   imports: [
     BrowserModule,
@@ -37,10 +39,11 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
     {
       provide: AuthHttp,
       useFactory: authHttpServiceFactory,
-      deps: [ Http, RequestOptions ]
+      deps: [Http, RequestOptions]
     },
     Auth
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
