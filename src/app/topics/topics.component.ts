67225abe55815eb8a6eeefed103e22a1d137fc98
Eugene Stativka <stativkaea@gmail.com>
2017-05-19 11:57:00 +0300

    Update voting and add sorting (#21)
    * Update voting mechanism
    
    * Add sorting

--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -1,10 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {TopicsService} from '../services/topics-service/topics.service';
-import {Auth} from '../services/auth-service/auth.service';
-import {SpinnerService} from '../services/spinner-service/spinner.service';
+import { Component, OnInit } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
+import { TopicsService } from '../services/topics-service/topics.service';
+import { Auth } from '../services/auth-service/auth.service';
+import { SpinnerService } from '../services/spinner-service/spinner.service';
 import NewTopicProps from './new-topic-props.interface';
 import Topic from './topic.interface';
-import {MdSnackBar} from '@angular/material';
+import VoteDirection from './vote-direction.type';
+
+enum SortOrders { None, Ascending, Descending }
 
 @Component({
   selector: 'app-topics',
@@ -12,7 +15,43 @@ import {MdSnackBar} from '@angular/material';
   styleUrls: ['./topics.component.scss'],
 })
 export class TopicsComponent implements OnInit {
-  topics: Topic[];
+  public topics: Topic[];
+
+  static sortTopics(topics: Topic[], sortField: 'name' | 'points', sortOrder: SortOrders): Topic[] {
+    switch (sortOrder) {
+      case SortOrders.None:
+        return topics;
+
+      case SortOrders.Ascending:
+        switch (sortField) {
+          case 'name':
+            return topics.sort((a: Topic, b: Topic): SortOrders => TopicsComponent.compareStrings(a.name, b.name));
+
+          case 'points':
+            return topics.sort((a: Topic, b: Topic): SortOrders => TopicsComponent.compareNumbers(a.points, b.points));
+        }
+        break;
+
+      case SortOrders.Descending:
+        switch (sortField) {
+          case 'name':
+            return topics.sort((a: Topic, b: Topic): SortOrders => TopicsComponent.compareStrings(b.name, a.name));
+
+          case 'points':
+            return topics.sort((a: Topic, b: Topic): SortOrders => TopicsComponent.compareNumbers(b.points, a.points));
+        }
+    }
+  }
+
+  static compareNumbers(a: number, b: number): SortOrders {
+    return b - a;
+  }
+
+  static compareStrings(a: string, b: string): SortOrders {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    return b > a ? 1 : b === a ? 0 : -1;
+  }
 
   constructor(
     private topicsService: TopicsService,
@@ -25,10 +64,11 @@ export class TopicsComponent implements OnInit {
     this.topicsService.getTopics()
       .subscribe(
         topics => {
-          this.topics = topics;
+          this.topics = TopicsComponent.sortTopics(topics, 'points', 0);
         },
         error => {
           this.snackBar.open('Cannot receive topics', 'close', { duration: 3000 });
+          this.spinner.toggleVisible(false);
           console.error(error);
         },
         () => this.spinner.toggleVisible(false),
@@ -46,33 +86,44 @@ export class TopicsComponent implements OnInit {
         },
         error => {
           this.snackBar.open('Cannot add new topic', 'close', { duration: 3000 });
+          this.spinner.toggleVisible(false);
           console.error(error);
         },
         () => this.spinner.toggleVisible(false),
       );
   }
 
-  // @TODO refactor voting mechanism
-  vote(direction: string, id: string) {
+  vote(direction: VoteDirection, id: string) {
     this.spinner.toggleVisible(true);
 
-    const points = this.topics.find(topic => topic._id === id).points + direction === 'up' ? 1 : -1;
-
-    this.topicsService.updateTopicById(id, { points })
+    this.topicsService.updateTopicPointsById(id, direction)
       .subscribe(
         updatedTopic => {
-          this.topics = this.topics.map(topic => {
-            if (topic._id === updatedTopic.id) {
-              topic.points = updatedTopic.points;
-            }
-            return topic;
-          });
+          this.topics = this.topics
+            .map(topic => {
+              if (topic._id === updatedTopic._id) {
+                topic.points = updatedTopic.points;
+
+                this.snackBar.open(`'${updatedTopic.name}' has been updated`, 'close', { duration: 3000 });
+              }
+              return topic;
+            });
         },
         error => {
           this.snackBar.open('Cannot save changes', 'close', { duration: 3000 });
+          this.spinner.toggleVisible(false);
           console.error(error);
         },
         () => this.spinner.toggleVisible(false),
       );
+  }
+
+  onSelectionChange(val) {
+    // @TODO find out what can be done
+    console.log(val);
+  }
+
+  onSortChange(val) {
+    this.topics = TopicsComponent.sortTopics(this.topics, val.sortBy, val.sortType);
   }
 }
