27044cc9297e49ec179757d2111e771e476ec818
Eugene Stativka <stativkaea@gmail.com>
2017-05-29 18:57:49 +0300

    Add speaker dropdown

--- src/app/topics/topic.interface.ts
+++ src/app/topics/topic.interface.ts
@@ -1,9 +1,10 @@
 interface Topic {
-  name: string;
-  likes: number;
-  usersLikedIds: string[],
-  likedByUser?: boolean,
-  _id: string;
+  _id: string; // database id
+  likes: number; // likes count
+  likedByUser?: boolean, // liked by current user (if any)
+  name: string; // topic's name
+  speakerId?: string, // id of the speaker (if any)
+  usersLikedIds: string[], // ids of users, who liked this topic or empty array
 }
 
 export default Topic;
---
+++ src/app/user/user.interface.ts
@@ -0,0 +1,8 @@
+interface User {
+  name: string; // full name
+  sub: string; // google auth's id
+  _id: string, // database id
+  picture: string; // link to google's avatar image
+}
+
+export default User;
