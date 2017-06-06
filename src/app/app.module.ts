f2fb200ee747330438f2c0f96ec06c80d9ac93f8
Eugene Stativka <stativkaea@gmail.com>
2017-06-06 18:36:51 +0300

    Reuse edit popup for addition of topics (#43)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import {AuthConfig, AuthHttp} from 'angular2-jwt';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {TopicsComponent} from './topics/topics.component';
-import {TopicAddPopupComponent} from './topic-add-popup/topic-add-popup.component';
 
 import {AuthService} from './services/auth-service/auth.service';
 import {AuthGuardService} from './services/auth-service/auth-guard.service';
@@ -41,7 +40,7 @@ import { UserService } from './services/user-service/user.service';
 import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { TopicDeletePopupComponent } from './topic-delete-popup/topic-delete-popup.component';
-import { TopicEditPopupComponent } from './topic-edit-popup/topic-edit-popup.component';
+import { TopicAddOrEditPopupComponent } from './topic-add-or-edit-popup/topic-add-or-edit-popup.component';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp(new AuthConfig({
@@ -55,17 +54,15 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
     AppComponent,
     HeaderComponent,
     TopicsComponent,
-    TopicAddPopupComponent,
     TopicDeletePopupComponent,
-    TopicEditPopupComponent,
+    TopicAddOrEditPopupComponent,
     UserComponent,
     UserDropdownComponent,
     DatepickerComponent,
   ],
   entryComponents: [
-    TopicAddPopupComponent,
     TopicDeletePopupComponent,
-    TopicEditPopupComponent,
+    TopicAddOrEditPopupComponent,
   ],
   imports: [
     BrowserModule,
